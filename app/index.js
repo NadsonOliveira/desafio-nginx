ff1556e59479ac92b879eff66372ccd4ead34278
Dromo <queiroz.nadson@greenbelt-ti.com>
2024-11-06 08:29:50 -0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,35 @@
+const express = require('express'); 
+const app = express(); 
+const port = 3000; 
+const config = { 
+    host: 'db', 
+    user: 'root', 
+    password: 'root', 
+    database: 'node_db' 
+}; 
+
+const mysql = require('mysql'); 
+
+app.get('/', (req, res) => { 
+
+    const connection = mysql.createConnection(config); 
+    connection.query(`insert into peoples(name) values('Nadson Oliveira');`);
+
+    let ul = ''; 
+    let li = ''; 
+
+    const result = connection.query("select * from peoples", (err, data) => { 
+        console.log(data); 
+        if (data && data.length) { 
+            data.map(element => { 
+                li += `<li>${element.id} - ${element.name}</li>`; 
+            }); 
+            ul = `<ul>${li}</ul>`; 
+        }
+        res.send(`<h1>Full Cycle Rocks!</h1>${ul}`); 
+    }); 
+    connection.end(); 
+
+}); 
+
+app.listen(port, () => console.log(`Rodando na porta ${port}`)); 
